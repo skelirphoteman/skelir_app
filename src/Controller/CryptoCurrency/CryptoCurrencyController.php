e6dcfaee6b33ba286f3e9359d50153129e50fd46
Skelirphoteman <galoin.julien@gmail.com>
2022-09-07 16:24:35 +0200

    Add CryptoCurrency List

--- src/Controller/CryptoCurrency/CryptoCurrencyController.php
+++ src/Controller/CryptoCurrency/CryptoCurrencyController.php
@@ -5,6 +5,8 @@ namespace App\Controller\CryptoCurrency;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -16,14 +18,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class CryptoCurrencyController extends AbstractController
 {
     /**
-     * @Route("/", "crypto_currency")
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/", "crypto_currency", methods={"GET"})
+     * @return Response
      *
      */
-    public function listCryptoCurrency()
+    public function indexCryptoCurrency()
     {
         return $this->render('app.html.twig', [
             'vueComponent'  => 'CryptoCurrency'
+        ]);
+    }
+
+    /**
+     * @Route("/list", "crypto_currency_list", methods={"GET"})
+     * @return JsonResponse
+     */
+    public function listCryptoCurrency()
+    {
+
+        return new JsonResponse([
+            "UTC/BTC"   => 'TEST'
         ]);
     }
 
